bd753e46ce22b6ae64b69cbe54e2bd8f5e80f87e
hasa-a-a <hassan_ali_ahmad@outlook.com>
2023-08-27 09:58:04 +0300

    add new js

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+let registerUserEl = document.getElementById ("registerUser")
+let registerPassEl = document.getElementById ("registerPass")
+let registerconfEl = document.getElementById ("registerconf")
+let submitEl = document.getElementById ("submit")
+let olduserEl = document.getElementById ("olduser")
+document.title = "USERS"

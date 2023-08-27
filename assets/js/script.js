0ff5fa246b3336c1d7060353eb2520740995b542
hasa-a-a <hassan_ali_ahmad@outlook.com>
2023-08-27 14:22:35 +0300

    add new function

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,4 +3,16 @@ let registerPassEl = document.getElementById ("registerPass")
 let registerconfEl = document.getElementById ("registerconf")
 let submitEl = document.getElementById ("submit")
 let olduserEl = document.getElementById ("olduser")
+const users = []
 document.title = "USERS"
+submitEl.addEventListener("click",function(){
+    if (registerUserEl.value == "" || registerPassEl.value == "") {
+        alert ("Please fill in all fields.")
+    }else if (registerconfEl.value !== registerPassEl.value ){
+        alert ("Your password dose not match")
+        return;
+    }else if (registerconfEl.value === registerPassEl.value){
+        alert ("You succefuly registred")
+        return;
+    }
+})
